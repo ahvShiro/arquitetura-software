69d5eb144648c82fdf0d7d543d898548f30eeab9
Arthur Heiji Voos Shiroshima <arthurshiro123@gmail.com>
2026-09-22 12:32:09 -0300

    [Driver: Arthur] [Navigator: Individual] fix: Schema alterado conforme especificações no documento

--- microsservicos/clientes/server.js
+++ microsservicos/clientes/server.js
@@ -9,33 +9,37 @@ app.use(express.json());
 
 app.get("/clientes", async (req, res) => {
   try {
-
     const result = await db.query("SELECT * FROM clientes ORDER BY id");
-    res.json(res.rows);
 
+    if (result.rowCount <= 0) {
+      return res.status(404).json({ erro: "Clientes não encontrados" });
+    }
+
+    res.json(result.rows);
   } catch (e) {
-
     res.status(500).json({
       erro: "Erro ao buscar clientes",
     });
-
   }
 });
 
+
 async function criarTabela() {
-    await db.query(`
+  await db.query(`
         CREATE TABLE IF NOT EXISTS clientes (
-        id SERIAL PRIMARY KEY,
-        nome VARCHAR(100) NOT NULL,
-        cpf VARCHAR(11) NOT NULL
-        )
+          id SERIAL PRIMARY KEY,
+          nome VARCHAR(100) NOT NULL,
+          sobrenome VARCHAR(100) NOT NULL,
+          telefone VARCHAR(20),
+          email VARCHAR(150) NOT NULL UNIQUE
+        );
     `);
 
-    console.log("Tabela de produtos pronta");
+  console.log("Tabela de clientes pronta");
 }
 
 criarTabela();
 
 app.listen(3003, () => {
-    console.log("Clientes rodando na porta 3003");
+  console.log("Clientes rodando na porta 3003");
 });
